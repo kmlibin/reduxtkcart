efee3539f9e94014bbea31e1aba4a4bb24ecaac9
Kelli Libin <km.putnam2@gmail.com>
2023-02-23 10:31:17 -0700

    added remove cart functionality

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,44 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        itemsList: [],
-        totalQuantity: 0,
-        showCart: false
+  name: "cart",
+  initialState: {
+    itemsList: [],
+    totalQuantity: 0,
+    showCart: false,
+  },
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+      console.log(newItem.price);
+      //check if item is present, then add to quantity
+      const existingItem = state.itemsList.find(
+        (item) => item.id === newItem.id
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+        state.totalQuantity++;
+      } else {
+        state.itemsList.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+        });
+        state.totalQuantity++;
+      }
     },
-    reducers: {
-        addToCart(state, action) {
-            const newItem = action.payload
-            console.log(newItem.price)
-            //check if item is present, then add to quantity
-            const existingItem = state.itemsList.find(item => item.id ===newItem.id);
-            if(existingItem) {
-                existingItem.quantity++
-                existingItem.totalPrice+= newItem.price
-                state.totalQuantity++
+    removeFromCart(state, action) {
+      //payload sends the id
+      const id = action.payload;
 
-            } else {
-                state.itemsList.push( {
-                    id: newItem.id,
-                    price: newItem.price,
-                    quantity: 1,
-                    totalPrice: newItem.price,
-                    name: newItem.name
-                })
-                state.totalQuantity++
-            }
-            
+      //checks if the id is in the itemsList
+      const existingItem = state.itemsList.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
 
-        },
-        removeFromCart() {
-
-        },
-        setShowCart(state) {
-            state.showCart = !state.showCart;
-        }
-    }
-})
+        //we only want items to be on the list if they are not equal to the payload id
+        state.itemsList = state.itemsList.filter((item) => item.id !== id);
+        state.totalQuantity--
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+        state.totalQuantity--
+      }
+    },
+    setShowCart(state) {
+      state.showCart = !state.showCart;
+    },
+  },
+});
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
