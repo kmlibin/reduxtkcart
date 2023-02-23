aabefac1b7f4fa86ffaaa8a8e8766a2f09a0cb07
Kelli Libin <km.putnam2@gmail.com>
2023-02-23 12:07:06 -0700

    install materialui, create ui state for alerts

---
+++ src/components/Notification.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Alert} from '@mui/material';
+
+const Notification = ({type, message}) => {
+  return (
+    <div><Alert severity={type}>{message}</Alert></div>
+  )
+}
+
+export default Notification
