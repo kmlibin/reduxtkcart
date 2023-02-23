7e03852afc696b8dad09e383e28a0cc4b2fe3e69
Kelli Libin <km.putnam2@gmail.com>
2023-02-23 16:09:00 -0700

    converted to thunks, left old code as comment.

--- src/App.js
+++ src/App.js
@@ -5,61 +5,77 @@ import Layout from "./components/Layout";
 //grabs from store
 import { useDispatch, useSelector } from "react-redux";
 import Notification from "./components/Notification";
-import { uiActions } from "./store/ui-slice";
+
+
+import { fetchData, sendCartData } from "./store/cart-actions";
 
 //checks if it is first render, then stops the useeffect from rendering the alert that pops up right away.
 let isFirstRender = true;
 function App() {
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn); 
+
   //grab cart state, then send https rquest
   const cart = useSelector((state) => state.cart);
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+ 
+ 
+//there is an issue with this useeffect
+  // useEffect(()=> {
+  //   dispatch(fetchData())
+  // },[dispatch]);
 
+  //this useEffect is hooked up to a thunk...the commented out part is how you can do it without a thunk
   useEffect(() => {
     if(isFirstRender) {
       isFirstRender = false;
-      return
+      return;
+    }
+    if(cart.changed) {
+      dispatch(sendCartData(cart))
     }
     //update state for notification
-    dispatch(
-      uiActions.showNotification({
-        open: true,
-        message: "sending request",
-        type: "warning",
-      })
-    );
+    // dispatch(
+    //   uiActions.showNotification({
+    //     open: true,
+    //     message: "sending request",
+    //     type: "warning",
+    //   })
+    // );
     //created a realtime database, updated rules, then grabbed url for the new created api. added 'cartItems.json'. .json always needs to be there
-    const sendRequest = async () => {
-      const response = await fetch(
-        //sends state
-        "https://reduxcart-d3878-default-rtdb.firebaseio.com/cartItems.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      const data = await response.json();
-      //send state for notification success
-      dispatch(
-        uiActions.showNotification({
-          open: true,
-          message: "sent request successfully",
-          type: "success",
-        })
-      );
-    };
-    sendRequest().catch((error) => {
+    // const sendRequest = async () => {
+    //   const response = await fetch(
+    //     //sends state
+    //     "https://reduxcart-d3878-default-rtdb.firebaseio.com/cartItems.json",
+    //     {
+    //       method: "PUT",
+    //       body: JSON.stringify(cart),
+    //     }
+    //   );
+    //   const data = await response.json();
+    //   //send state for notification success
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       open: true,
+    //       message: "sent request successfully",
+    //       type: "success",
+    //     })
+    //   );
+    // };
+    // sendRequest().catch((error) => {
       //send state as error
-      dispatch(
-        uiActions.showNotification({
-          open: true,
-          message: "failed request",
-          type: "error",
-        })
-      );
-    });
-  }, [cart]);
+      // dispatch(
+      //   uiActions.showNotification({
+      //     open: true,
+      //     message: "failed request",
+      //     type: "error",
+      //   })
+      // );
+
+      //still have to dispatch the thunk function
+      //dispatch(sendCartData(cart))
+   
+  }, [cart, dispatch]);
 
   return (
     <div className="App">
